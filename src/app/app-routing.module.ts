4fe0d48d9db1587e20f38cfee1c0005fc2f86e6b
Daniel <danieljancar@bluewin.ch>
2023-01-03 08:52:51 +0100

    Added a working Router Guard and Login / Register Function

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,42 +1,50 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { redirectUnauthorizedTo, redirectLoggedInTo, canActivate } from '@angular/fire/auth-guard';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['']);
+const redirectLoggedInToHome = () => redirectLoggedInTo(['home']);
 
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule),
+    ...canActivate(redirectUnauthorizedToLogin)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
     path: 'friends',
-    loadChildren: () => import('./friends/friends.module').then( m => m.FriendsPageModule)
+    loadChildren: () => import('./friends/friends.module').then(m => m.FriendsPageModule),
+    ...canActivate(redirectUnauthorizedToLogin)
   },
   {
     path: 'settings',
-    loadChildren: () => import('./settings/settings.module').then( m => m.SettingsPageModule)
+    loadChildren: () => import('./settings/settings.module').then(m => m.SettingsPageModule),
+    ...canActivate(redirectUnauthorizedToLogin)
   },
   {
     path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule),
+    ...canActivate(redirectLoggedInToHome)
   },
   {
     path: 'register',
-    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+    loadChildren: () => import('./register/register.module').then(m => m.RegisterPageModule),
+    ...canActivate(redirectLoggedInToHome)
   },
   {
     path: 'new-note',
-    loadChildren: () => import('./new-note/new-note.module').then( m => m.NewNotePageModule)
+    loadChildren: () => import('./new-note/new-note.module').then(m => m.NewNotePageModule),
+    ...canActivate(redirectUnauthorizedToLogin)
   },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,30 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'neonote-e776f',
+    appId: '1:1004925165701:web:48d026e01dec816f4089ec',
+    storageBucket: 'neonote-e776f.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDY2QQZnDo27ys0cTkdS6BeQry8aQCe1YI',
+    authDomain: 'neonote-e776f.firebaseapp.com',
+    messagingSenderId: '1004925165701',
+    measurementId: 'G-0T6VVS6G21',
+  },
   production: false
 };
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDY2QQZnDo27ys0cTkdS6BeQry8aQCe1YI",
-  authDomain: "neonote-e776f.firebaseapp.com",
-  projectId: "neonote-e776f",
-  storageBucket: "neonote-e776f.appspot.com",
-  messagingSenderId: "1004925165701",
-  appId: "1:1004925165701:web:48d026e01dec816f4089ec",
-  measurementId: "G-0T6VVS6G21"
-};
-
-// Initialize Firebase
-const firebase_db = initializeApp(firebaseConfig);
-const analytics = getAnalytics(firebase_db);
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
