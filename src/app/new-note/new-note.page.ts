283a4f4ffc3adfbc3126d3435fbe626717f0ab7b
Daniel <danieljancar@bluewin.ch>
2023-01-04 15:37:18 +0100

    Small UI changes

--- src/app/new-note/new-note.page.ts
+++ src/app/new-note/new-note.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AlertController, LoadingController } from '@ionic/angular';
+import { AlertController, LoadingController, ActionSheetController } from '@ionic/angular';
 import { delay } from 'rxjs';
 import { AuthService } from '../_services/auth.service';
 import { DataService } from '../_services/data.service';
@@ -12,6 +12,7 @@ import { DataService } from '../_services/data.service';
   styleUrls: ['./new-note.page.scss'],
 })
 export class NewNotePage implements OnInit {
+  result!: string;
 
   constructor(
     private fb: FormBuilder,
@@ -19,8 +20,42 @@ export class NewNotePage implements OnInit {
     private alertController: AlertController,
     private authService: AuthService,
     private router: Router,
-    private UserService: DataService
+    private UserService: DataService,
+    private actionSheetCtrl: ActionSheetController
   ) { }
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Add new banner',
+      subHeader: 'Please upload or take a new banner to add to your note',
+      buttons: [
+        {
+          text: 'Upload from device',
+          data: {
+            action: 'uploadDevice',
+          },
+        },
+        {
+          text: 'Take with camera',
+          data: {
+            action: 'takeCamera',
+          },
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          data: {
+            action: 'cancel',
+          },
+        },
+      ],
+    });
+
+    await actionSheet.present();
+
+    const result = await actionSheet.onDidDismiss();
+    this.result = JSON.stringify(result, null, 2);
+  }
 
   async chooseImage() {
     const alert = await this.alertController.create({
@@ -68,6 +103,25 @@ export class NewNotePage implements OnInit {
 
     await alert.present();
   }
+
+  async chooseDelete() {
+    const alert = await this.alertController.create({
+      header: 'Do you really want to delete this note?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+        {
+          text: 'Yes',
+          role: 'confirm',
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
 
   ngOnInit() {
   }
