1080a8d5563ca29a67522f1bbd50c101850f9bdd
Daniel <danieljancar@bluewin.ch>
2023-01-07 14:13:59 +0100

    Get specified note data function added

--- src/app/view-note/view-note.page.ts
+++ src/app/view-note/view-note.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { getAuth } from '@angular/fire/auth';
 import { collection, doc, docData, DocumentReference, Firestore, getDoc, getDocFromServer } from '@angular/fire/firestore';
 import { ActivatedRoute } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 import { Observable, shareReplay, Subscription, take, tap } from 'rxjs';
 @Component({
   selector: 'app-view-note',
@@ -10,8 +11,8 @@ import { Observable, shareReplay, Subscription, take, tap } from 'rxjs';
 })
 export class ViewNotePage implements OnInit {
   noteId!: string;
-  
-  constructor(private route: ActivatedRoute, private firestore: Firestore) {
+
+  constructor(private route: ActivatedRoute, private firestore: Firestore, private loadingController: LoadingController,) {
 
   }
   ngOnInit() {
@@ -24,10 +25,17 @@ export class ViewNotePage implements OnInit {
   }
 
   ionViewDidEnter() {
-    console.log(this.noteId);
+    this.getNote()
   }
 
-  getNote() {
-
+  async getNote() {
+    const loading = await this.loadingController.create();
+    await loading.present();
+    const auth = getAuth();
+    const user = auth.currentUser;
+    const noteRef = doc(this.firestore, `users/${user?.uid}/notes/${this.noteId}`)
+    const noteGet = await getDocFromServer(noteRef)
+    const noteInfo = await noteGet.data();
+    await loading.dismiss();
   }
 }
